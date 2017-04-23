b64cdd857248002edc4c1d10296d7afdc43018a8
loiane <loianeg@gmail.com>
2017-04-23 12:30:21 -0300

    update main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   title = 'app works!';
+  users: any[] = [];
+
+  constructor(){
+    for(let i=0; i<5000; i++){
+      let num = i + 1;
+      this.users.push({
+        name: 'user ' + num,
+        prop1: num,
+        prop2: num,
+        prop3: num,
+        prop4: num,
+        prop5: num
+      })
+    }
+  } 
+
+  changeTitle() { 
+    this.title = 'App title updated' ;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { UsersListComponent } from './users-list/users-list.component';
+import { UserListItemComponent } from './user-list-item/user-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersListComponent,
+    UserListItemComponent
   ],
   imports: [
     BrowserModule,
